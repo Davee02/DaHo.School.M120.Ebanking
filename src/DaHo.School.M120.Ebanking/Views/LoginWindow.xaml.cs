c6e77d02611a111dbb42f498c2bbda4b64d5d2f9
David Hodel <davidhodel6@gmail.com>
2019-05-31 10:43:30 +0200

    Renamed Window

--- src/DaHo.School.M120.Ebanking/Views/LoginWindow.xaml.cs
+++ src/DaHo.School.M120.Ebanking/Views/LoginWindow.xaml.cs
@@ -3,11 +3,11 @@
 namespace DaHo.School.M120.Ebanking.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for LoginWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginWindow : Window
     {
-        public MainWindow()
+        public LoginWindow()
         {
             InitializeComponent();
         }
